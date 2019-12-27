b1d4dcec99edba4f38352023e34d8031034d6904
Mateusz <skaua@o2.pl>
2019-12-27 22:03:23 +0100

    Add BookingSummary

---
+++ frontend/src/components/BookingSummary/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'css/BookingSummary.css';
+import { convertTimestamp } from 'utils/timestampConverter'
+
+const BookingSummary = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <img className="img-thumbnail mx-auto d-block img-fluid"
+                        alt={props.releaseDate}
+                        src={props.image} />
+                </div>
+                <div className="col-8">
+                    <div className="header">
+                        Your booking summary
+</div>
+                    <span className="summary-title">Movie Title: {props.title}</span>
+                    <div className="time">Reservation Time: {convertTimestamp(props.reservationTime)}</div>
+                    <div className="text">Seats Booked: {props.seats.map((item, key) =>
+                        <li>{`${item.row}${item.column}`}</li>
+                    )}</div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BookingSummary;
