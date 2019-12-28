bcb0b82d7f67085ad295395f8cf471debb312d8d
Marta Jaszewska <jaszewska.marta@gmail.com>
2019-12-28 19:46:07 +0100

    grid-fix

--- frontend/src/components/MovieDetails/index.jsx
+++ frontend/src/components/MovieDetails/index.jsx
@@ -17,30 +17,39 @@ const MovieDetails = (props) => {
                     src={props.image} />
                 </div>
                 <div className="col-8">
-                    <div class="content">
-                        <div className="movie-details-title">
-                            <span>{props.title}</span>
-                            <span className="vote"><i class="fas fa-star"></i> {props.vote}</span>
-                            <br />
-                            <span className="vote">Average rating</span>
+                    {/* <div class="content"> */}
+                        <div className="movie-details-title row">
+                            <div className="col">
+                                <span>{props.title}</span>
+                            </div>
+
+                            <div className="col">
+                                <span className="vote"><i class="fas fa-star"></i> {props.vote}</span>
+                                <br />
+                                <span className="vote">Average rating</span>
+                            </div>
                         </div>
-                        <div>
+                        <div className="row">
                             <span className="text">Relase Date:  </span>
                             <div> {props.releaseDate}</div>
                         </div>
-                        <div>
+                        <div className="row">
                             <span className="text">Description: </span>
                             <div className="description">{props.content}</div>
                         </div>
-                        <iframe className="trailer"
-                            src={`https://www.youtube.com/embed/${props.trailers}?rel=0&amp;showinfo=0`}
-                            frameBorder="0"
-                            title={props.title}>
-                        </iframe>
-                    </div>
-                    <div>
+                    {/* </div> */}
+                    <div className="row">
+                        <div className="col">
+                            <iframe className="trailer"
+                                src={`https://www.youtube.com/embed/${props.trailers}?rel=0&amp;showinfo=0`}
+                                frameBorder="0"
+                                title={props.title}>
+                            </iframe>
+                        </div>
+                    <div className="col d-flex flex-column justify-content-center align-items-center">
                         <Link to={dateSelection(id)}><button className="btn">BOOK</button></Link>
                         <Link to={homepage}><button className="btn">GO BACK</button></Link>
+                    </div>
                     </div>
                 </div>
             </div>
